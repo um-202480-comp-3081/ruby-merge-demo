cc5fae39b4c84bd2bb333b0aac749eff01215381
Miguel Perez <migeperezjr@gmail.com>
2024-10-07 20:54:00 -0500

    Added admin functionality to user class

--- user.rb
+++ user.rb
@@ -1,17 +1,22 @@
-# user.rb
+# user.rb (feature-admin branch)
 class User
-    attr_reader :name, :email
+    attr_reader :name, :email, :admin
     
-    def initialize(name, email)
+    def initialize(name, email, admin = false)
         @name = name
         @email = email
+        @admin = admin
     end
     
     def display_info
-        "#{name} (#{email})"
+        "#{name} (#{email})#{' - Admin' if admin}"
     end
     
     def valid?
         name.length > 0 && email.include?('@')
+    end
+    
+    def can_access_admin_panel?
+        admin
     end
 end
